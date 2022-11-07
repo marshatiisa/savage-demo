672dac878b52a0e3a0421a3a3038a1812863d341
Marsha <marshatiisa35@gmail.com>
2022-11-06 23:03:59 -0500

    fixed bugs. got likes to work

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ const MongoClient = require('mongodb').MongoClient
 
 var db, collection;
 
-const url = "mongodb+srv://demo:demo@cluster0-q2ojb.mongodb.net/test?retryWrites=true";
+// const url = "mongodb+srv://demo:demo@cluster0-q2ojb.mongodb.net/test?retryWrites=true";
+const url = "mongodb+srv://marshatiisa:ZjTYmNmnQQtcz5@cluster0.8gmpf0f.mongodb.net/?retryWrites=true&w=majority"
 const dbName = "demo";
 
 app.listen(3000, () => {
@@ -14,6 +15,7 @@ app.listen(3000, () => {
             throw error;
         }
         db = client.db(dbName);
+        console.log(db)
         console.log("Connected to `" + dbName + "`!");
     });
 });
@@ -31,7 +33,7 @@ app.get('/', (req, res) => {
 })
 
 app.post('/messages', (req, res) => {
-  db.collection('messages').insertOne({name: req.body.name, msg: req.body.msg, thumbUp: 0, thumbDown:0}, (err, result) => {
+  db.collection('messages').insertOne({name: req.body.name, msg: req.body.msg, totalLikes: 0 }, (err, result) => {
     if (err) return console.log(err)
     console.log('saved to database')
     res.redirect('/')
@@ -39,10 +41,26 @@ app.post('/messages', (req, res) => {
 })
 
 app.put('/messages', (req, res) => {
+  console.log(req.body)
   db.collection('messages')
   .findOneAndUpdate({name: req.body.name, msg: req.body.msg}, {
     $set: {
-      thumbUp:req.body.thumbUp + 1
+      totalLikes:req.body.likes + 1
+    }
+  }, {
+    sort: {_id: -1},
+    upsert: true
+  }, (err, result) => {
+    if (err) return res.send(err)
+    res.send(result)
+  })
+})
+
+app.put('/thumbDown', (req, res) => {
+  db.collection('messages')
+  .findOneAndUpdate({name: req.body.name, msg: req.body.msg}, {
+    $set: {
+      totalLikes:req.body.thumbDown > 0 ? req.body.thumbDown - 1 : 0
     }
   }, {
     sort: {_id: -1},
